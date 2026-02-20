adc082e3aac2e8fde335939cdab61db0460c6774
Silje Albrechtsen <saalbrec@stud.ntnu.no>
2026-02-20 14:13:33 +0100

    Sett litt på synkronisering og struktur

---
+++ Project/synchronization.go
@@ -0,0 +1,12 @@
+// TODO
+
+/*
+
+Må sammenligne worldviews
+
+Kjøre clear at curret floor. Kan brukes til state til foreslått slettet
+
+Må kjøre cost-func (timeToIdle) når den får en ny ordre
+
+
+*/
