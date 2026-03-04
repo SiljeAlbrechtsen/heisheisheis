8c9be241e8397edd690b2ea36bac7e94d448efb9
Ingrid Skålnes Strømme <94357799+ingstro@users.noreply.github.com>
2026-03-04 09:27:42 +0100

    Update synchronization.go

--- Project/synchronization.go
+++ Project/synchronization.go
@@ -2,11 +2,14 @@
 
 /*
 
-Må sammenligne worldviews
+Må:
+- Få inn heartbeats
+- Må sammenligne worldviews
+- Hvis like ...
+- Hvis ikke like ...
 
 Kjøre clear at curret floor. Kan brukes til state til foreslått slettet
 
 Må kjøre cost-func (timeToIdle) når den får en ny ordre
-
 
 */
