87d3ee8c7bd06fca0fed9bb7b56275cdd913d539
Silje Albrechtsen <saalbrec@stud.ntnu.no>
2026-03-04 14:21:56 +0100

    merget branches

--- Project/worldview.go
+++ Project/worldview.go
@@ -6,26 +6,29 @@ type Directions uint8 = 2
 //type CabOrders [NumFloors]bool // Må vel ikke deklareres først?
 
 
-
 const (
     None OrderState = iota
     Unconfirmed
     Confirmed
     DeleteProposed
+	noOrders
 )
 
 type HallOrders [NumFloors][Directions]OrderState
 
+type StateElevator = Elevator
+
 // Struct for egen worldview
 type WorldView struct {
 	IDelevator uint8
-	StateElevator // TODO: få inn fra fsm
-	HallOrders HallOrders
-	CabOrders [NumFloors]bool // En liste med true or false for hver eneste etasje å trykke inn
+	StateElevator // TODO: Må hente type fra fsm
+	hallOrders HallOrders
+	mycabOrders [NumFloors]bool // En liste med true or false for hver eneste etasje å trykke inn
 }
 
 // Struct der alle sine worldviews 
 type MergedWorldviews struct {
 	Elevators map[ElevID]ElevState
-
 }
+
+
